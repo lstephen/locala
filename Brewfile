5aa717dcc94c29e1ab10670d3c92d8bbf0b6040b
lstephen <levi.stephen@gmail.com>
2017-11-10 11:22:13 -0800

    Switch to brew bundle

---
+++ Brewfile
@@ -0,0 +1,26 @@
+
+tap 'caskroom/cask'
+tap 'caskroom/fonts'
+
+brew 'doxygen'
+brew 'gnu-tar'
+brew 'git'
+brew 'git-lfs'
+brew 'python2'
+brew 'python3'
+brew 'reattach-to-user-namespace'
+brew 'the_silver_searcher'
+brew 'tmux'
+brew 'vim'
+
+cask 'docker'
+cask 'font-ubuntu'
+cask 'google-chrome'
+cask 'iterm2'
+cask 'java'
+cask 'kindle'
+cask 'macvim'
+cask 'skype'
+cask 'spectacle'
+cask 'wineskin-winery'
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,18 @@ then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install gnu-tar python2
+echo "Updating brew..."
+brew update
+
+echo "Verifying brew..."
+brew doctor
+
+echo "Packages not listed in Brewfile..."
+brew bundle cleanup
+
 pip2 install ansible==2.2.1.0
+
+echo "Configuring..."
 ansible-playbook all.yml $ANSIBLE_FLAGS
+
+echo "Done."
